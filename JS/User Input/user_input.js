3434b8a3393ef8c5a4c9c245c49eb5c847069621
Timothy Enderson <timothyenderson01@gmail.com>
2023-02-06 08:48:58 +0100

    User Input Part One

---
+++ JS/User Input/user_input.js
@@ -0,0 +1,21 @@
+/*
+    There Is An Easy Way To Accept Input => Which Is Not The Recommended Way
+
+    And There Is One That Requires A Bit More Setup
+
+    Let's Focus On The User Prompt First (Easy Mode)
+*/
+
+/* UNCOMMENT TO SEE DEMO
+let input = window.prompt("Gimme Some Input, PLEASE!");
+
+if (input.length !== 0)
+  if (input.length <= 57) {
+    document.getElementById("wpo").innerText += " " + input;
+  } else {
+    document.getElementById("wpo").innerText += " Input Too Long";
+  }
+else {
+  document.getElementById("wpo").innerText += " Nice Try ;)";
+}
+*/
