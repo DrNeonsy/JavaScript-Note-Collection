6812c145b6b6c5604f5b03c2c212c4d0203639b5
Timothy Enderson <timothyenderson01@gmail.com>
2023-02-07 14:29:16 +0100

    Multidimensional Array Done

---
+++ JS/Multidimensional Arrays/multidimensional_arrays.js
@@ -0,0 +1,23 @@
+/*
+    A Multidimensional Array Might Sound Fancy But It Is Just An Array
+    That Is Holding Other Arrays
+
+    So It's "Just" An Array Of Arrays
+*/
+
+let reds = ["lightRed", "darkRed", "wineRed"];
+let blues = ["lightBlue", "cyan", "aqua", "darkBlue"];
+let greens = ["lightGreen", "darkGreen", "lime", "poisonGreen"];
+
+let colors = [reds, blues, greens];
+
+console.log("Colors Array", colors); // Open The Output Via The Arrow In The Console
+
+console.log("\nNow To Loop Through\n");
+
+for (const list of colors) {
+  console.log("\nArray Group:\n", list, "\nArray Items:"); // First Print The Full Array
+  for (const item of list) {
+    console.log(item); // Print Every Array Item Of That Array
+  }
+}
