02753dd993f6bf656c5a7194ff5ad1dc53da8f3c
Timothy Enderson <timothyenderson01@gmail.com>
2023-02-07 15:05:09 +0100

    Array Function Update

--- JS/Array Functions/array_functions.js
+++ JS/Array Functions/array_functions.js
@@ -1,8 +1,8 @@
-/*
-    ForEach
-    Map
-    Filter
-    Reduce
-    Sort Numbers
-    Sort Strings
-*/
+document.getElementById("Button").onclick = function () {
+  let forEach = document.getElementById("ForEach").value;
+  let map = document.getElementById("Map").value;
+  let filter = document.getElementById("Filter").value;
+  let reduce = document.getElementById("Reduce").value;
+  let sortNumbers = document.getElementById("Sort Numbers").value;
+  let sortStrings = document.getElementById("Sort Strings").value;
+};
