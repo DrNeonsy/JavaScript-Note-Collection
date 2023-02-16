0550628941de26fa617d33f5328dd33e49303ec8
Timothy Enderson <timothyenderson01@gmail.com>
2023-02-16 15:33:36 +0100

    Constructor Section Done

--- JS/Objects/Constructor/constructor.js
+++ JS/Objects/Constructor/constructor.js
@@ -1,9 +1,36 @@
 /*
     A Constructor Is A Unique Method Of A Class That Takes Arguments And Assigns Those To Properties
-    To Help Create A More Dynamic Set Of Objects, Something We Weren't Able Until Now (Based On Learning Steps)
+    To Help Create A More Dynamic Set Of p1s, Something We Weren't Able Until Now (Based On Learning Steps)
 
-    In Terms Of Naming Convention As Far As I Know The Constructor Starts With A Capital Letter
+    In Terms Of Naming Convention As Far As I Know The Constructor Starts With A Capital Letter And Can Be Named Whatever
     Though I Personally Prefer To Start All Methods With A Capital Letter So... 🆘
 */
 
+class Person {
+    constructor(name, age, favNum = null, favCol = null, favAnim = null) {
+        this.name = name;
+        this.age = age;
+        this.favNum = favNum;
+        this.favCol = favCol;
+        this.favAnim = favAnim;
+    }
+}
 
+const p1 = new Person("Timothy", 22, 5, "Blue", "Siberian tiger");
+
+/*
+    Using The ForIn Code Snippet From VSC We Can Output Our Properties Like This
+
+        for (const key in p1) {
+        if (p1.hasOwnProperty.call(p1, key)) {
+            const element = p1[key];
+            console.log(element);
+        }
+    }
+*/
+
+// Alternative We Can Just Do This
+
+for (const property in p1) {
+    console.log(`${property}: ${p1[property]}`);
+}
